720666957d19c6c423fe84bf2c64c4bcc75ea916
kali419 <120096560+kali419@users.noreply.github.com>
2023-04-30 22:50:58 +0100

    Add files via upload

---
+++ assignment.js
@@ -0,0 +1,106 @@
+
+
+// 1.
+//  let x = 1;
+
+// while (x <= 100) {
+//     if (x % 3 === 0 && x % 5 === 0) {
+//         console.log("FizzBuzz");
+//     } else if (x % 3 === 0) {
+//         console.log("Fizz");
+//     } else if (x % 5 === 0) {
+//         console.log("Buzz");
+//     } else {
+//         console.log(x);
+//     }
+//     x++;
+// }
+
+
+
+
+
+
+
+// 2. 
+// var num = 99;
+
+// while (num >= 1) {
+//     // check pluralization
+//     let bottleS = (num === 1) ? "" : "s";
+//     let nextBottleS = (num === 2) ? "" : "s";
+    
+//     // print lyrics
+//     console.log(num + " bottle" + bottleS + " of juice on the wall! " + num + " bottle" + bottleS + " of juice! Take one down, pass it around... " + (num-1) + " bottle" + nextBottleS + " of juice on the wall!");
+    
+//     // decrement num
+//     num--;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 4. 
+// let x = 9;
+// while (x >= 1) {
+//   console.log("hello " + x);
+//   x = x - 1;
+// }
+
+// for (let x = 9; x >= 1; x--) {
+//   console.log("hello " + x);
+// }
+
+
+
+
+
+// 5. 
+
+
+// fix the loop
+// for (let x = 5; x < 10; x++) {
+//   console.log(x)
+// }
+
+
+
+
+
+// 6. 
+// fix the loop
+// for (let k = 0; k < 200; k++) {
+//   console.log(k);
+// }
+
+
+
+
+// 7.
+// let solution = 1;
+// for (let i = 1; i <= 12; i++) {
+//   solution *= i;
+// }
+// console.log(solution);
+
+
+
+
+// 8.
+// for (let row = 0; row < 26; row++) {
+//   for (let seat = 0; seat < 100; seat++) {
+//     console.log(row + "-" + seat);
+//   }
+// }
+
+
+
