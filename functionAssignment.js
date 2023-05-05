1593b473d9ed8e281f7f25c9b6b87f72d35f0589
kali419 <120096560+kali419@users.noreply.github.com>
2023-05-05 14:57:35 +0100

    Add files via upload

---
+++ functionAssignment.js
@@ -0,0 +1,79 @@
+// 1.
+// function laugh() {
+//   return "hahahahahahahahahaha!";
+// }
+
+// console.log(laugh());
+
+
+// 2.
+// function laugh(num) {
+//     let laughter = "";
+//     for (let i = 0; i < num; i++) {
+//       laughter += "ha";
+//     }
+//     laughter += "!";
+//     return laughter;
+//   }
+  
+//   console.log(laugh(3));
+
+  
+
+
+// 3.
+// function buildTriangle(width) {
+//     var triangle = "";
+//     for (var i = 1; i <= width; i++) {
+//       triangle += makeLine(i);
+//     }
+//     return triangle;
+//   }
+  
+//   function makeLine(length) {
+//     var line = "";
+//     for (var j = 1; j <= length; j++) {
+//       line += "* ";
+//     }
+//     return line + "\n";
+//   }
+  
+//   console.log(makeLine(10));
+  
+
+// 4.
+// const laugh = function(num) {
+//     let laughString = "";
+//     for (let i = 0; i < num; i++) {
+//       laughString += "ha ";
+//     }
+//     return laughString + "!";
+//   }
+  
+//   console.log(laugh(3)); 
+  
+
+
+// 5
+// const cry = function cry() {
+//     return "boohoo!";
+//   }
+  
+//   console.log(cry());
+
+  
+
+
+// 6
+// function emotions(myString, myFunc) {
+//     console.log("I am " + myString + ", " + myFunc(2));
+//   }
+  
+// emotions("happy", function(num) {
+//     var laughter = "";
+//     for (var i = 0; i < num; i++) {
+//       laughter += "ha";
+//     }
+//     return laughter + "!";
+
+//   });
